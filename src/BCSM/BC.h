b286dd1de2b29fbdac0c0dba8f4a80dc9343c6c5
probshin <cheng172@purdue.edu>
2020-03-19 17:01:01 -0400

    some update :

--- src/BCSM/BC.h
+++ src/BCSM/BC.h
@@ -3,8 +3,42 @@
     You should have received a copy of the License. If not, see 
     <https://github.com/CSCsw/ColPack>
 *******************************************************************************/
+#ifndef BC_HPP
+#define BC_HPP
 
-
+#include<vector>
 #include<iostream>
-using namespace std;
+#include<omp.h>
+#include<map>
+#include<list>
+#include<set>
+#include<random>
+#include<chrono>
 
+
+using namespace std;
+namespace ColPack{
+
+//=========================================================================
+// class of BC
+// ----------------------------------------------------------------
+// 
+//=========================================================================
+class BC {
+public: chrono::duration<double> ChronoDuration;
+public:
+    static const int L = 1;
+    static const int R = 2;
+    static const int ROW = 1;
+    static const int COL = 2;
+    static const int LR  = 3;
+    static const int S = 1;
+    static const int T = 2;
+
+public:
+    static const 
+} // end of class BC
+
+
+} // end of namespace ColPack
+#endif
