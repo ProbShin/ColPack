c77eb083d16f260b7bc86fa0eabd4c06842c7939
probshin <cheng172@purdue.edu>
2020-03-19 16:43:18 -0400

    update

---
+++ src/BCSM/BC.h
@@ -0,0 +1,10 @@
+/*******************************************************************************
+    This file is part of ColPack, which is under its License protection.
+    You should have received a copy of the License. If not, see 
+    <https://github.com/CSCsw/ColPack>
+*******************************************************************************/
+
+
+#include<iostream>
+using namespace std;
+
